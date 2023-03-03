d3a9660e340593a424645defee56741f0958825c
Javier Carrasco <javierdeveloper08@gmail.com>
2023-03-03 18:15:29 -0400

    Adding postcss,autoprefixer

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,6 +1,5 @@
 const $layer = document.querySelector(".layer-exit");
 
 $layer.addEventListener("pointerdown", (e) => {
-  console.log("SI");
   e.target.parentElement.parentElement.classList.add("translate-y-[-100%]");
 });
